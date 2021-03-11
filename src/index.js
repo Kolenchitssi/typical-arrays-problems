5e1a1d190b0190336d41cd510e357f06d2b7ce5e
Kolenchitssi <kolenchitssi@gmail.com>
2021-03-11 12:18:05 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+
+    if (array == false || array.length < 1) {
+        return 0;
+    }
+
+    let res = Math.min(...array);
+    return res;
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+
+    if (array == false || array.length < 1) {
+        return 0;
+    }
+
+    let res = Math.max(...array);
+    return res;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+    if (array == false || array.length < 1) {
+        return 0;
+    }
+
+    let len = array.length;
+    let res = 0;
+
+    for (let i = 0; i < len; i++) {
+        res += array[i];
+    }
+
+    res = res / len;
+
+
+    return res;
 }
